88b081f36d915e5103fa5ed785db207eacd8b958
Jude Baca-Miller <judeamiller@gmail.com>
2018-10-31 21:33:48 -0600

    initial commit of map js file.

---
+++ JS/map.js
@@ -0,0 +1,6 @@
+var map = L.map('mapid').setView([37.9, -95], 4);
+
+L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);

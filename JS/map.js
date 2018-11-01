3af7c5e7a1cd1d1abf12671d5c7bfac7c9c07477
Jude Baca-Miller <judeamiller@gmail.com>
2018-11-01 15:01:20 -0600

    map now colorizes states that I have visited.

--- JS/map.js
+++ JS/map.js
@@ -4,3 +4,24 @@ L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+L.geoJson(statesVisited).addTo(map);
+
+function styleMap(v) {
+    return v === true  ? '#BADA55':
+                         '#c7c8c9';
+}
+
+
+
+function style (feature) {
+    return {
+        fillColor: styleMap(feature.properties.visited),
+        weight: 2,
+        opacity:  'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    }
+}
+
+L.geoJson(statesVisited, {style: style}).addTo(map);
